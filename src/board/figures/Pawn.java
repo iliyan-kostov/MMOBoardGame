a3a3e045bc75ab988faa341a0e07a05df0a0ffbc
iliyan-kostov <iliyan.kostov.gml@gmail.com>
2017-05-13 07:28:32 +0300

    Created classes for some game figures.

---
+++ src/board/figures/Pawn.java
@@ -0,0 +1,51 @@
+package board.figures;
+
+import javafx.scene.shape.Cylinder;
+import javafx.scene.shape.Sphere;
+
+/**
+ * <p>
+ * Клас за фигура, подобна на пешките от шаха, реализирана чрез прости JavaFX
+ * триизмерни примитиви.
+ *
+ * @author iliyan-kostov <https://github.com/iliyan-kostov/>
+ */
+public final class Pawn extends board.figures.Figure {
+
+    // настройки по подразбиране за размерите
+    private final static double DEFAULT_BASERADIUS = 9;
+    private final static double DEFAULT_BASEHEIGHT = 3;
+    private final static double DEFAULT_MIDDLERADIUS = 7;
+    private final static double DEFAULT_TOPRADIUS = 4;
+    // настройки по подразбиране за детайлността
+    private final static int DEFAULT_BASEDIVISIONS = 16;
+    private final static int DEFAULT_MIDDLEDIVISIONS = 16;
+    private final static int DEFAULT_TOPDIVISIONS = 16;
+
+    // съставни елементи на обема на фигурата:
+    private final Cylinder base;
+    private final Sphere middle;
+    private final Sphere top;
+
+    /**
+     * Конструктор без параметри.
+     */
+    public Pawn() {
+        super();
+        this.base = new Cylinder(Pawn.DEFAULT_BASERADIUS, Pawn.DEFAULT_BASEHEIGHT, Pawn.DEFAULT_BASEDIVISIONS);
+        this.middle = new Sphere(Pawn.DEFAULT_MIDDLERADIUS, Pawn.DEFAULT_MIDDLEDIVISIONS);
+        this.top = new Sphere(Pawn.DEFAULT_TOPRADIUS, Pawn.DEFAULT_TOPDIVISIONS);
+        this.base.setTranslateX(0);
+        this.base.setTranslateY(0);
+        this.base.setTranslateZ(0);
+        this.middle.setTranslateX(0);
+        this.middle.setTranslateY(0);
+        this.middle.setTranslateZ(0 - Pawn.DEFAULT_BASEHEIGHT);
+        this.top.setTranslateX(0);
+        this.top.setTranslateY(0);
+        this.top.setTranslateZ(0 - Pawn.DEFAULT_BASEHEIGHT - Pawn.DEFAULT_MIDDLERADIUS / 2);
+        this.getChildren().add(this.base);
+        this.getChildren().add(this.middle);
+        this.getChildren().add(this.top);
+    }
+}
