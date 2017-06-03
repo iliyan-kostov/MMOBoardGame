369d9395435f041f092d508f8594fceb9a41a69a
iliyan-kostov <iliyan.kostov.gml@gmail.com>
2017-06-03 19:37:13 +0100

    Fixed missing files in push.

---
+++ src/boardNew02/BoardCoords.java
@@ -0,0 +1,18 @@
+package boardNew02;
+
+/**
+ * <p>
+ * Координати на поле (или фигура) в рамките на дъската.
+ *
+ * @author iliyan-kostov <https://github.com/iliyan-kostov/>
+ */
+public final class BoardCoords {
+
+    public final int x;
+    public final int y;
+
+    public BoardCoords(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
